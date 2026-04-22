53834b08c463e946bd2d9b040ca759c14f319a84
LeProxy <notleproxy1337@gmail.com>
2026-04-22 13:30:56 -0400

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,20 +7,20 @@ struct thread_context_t {
 };
 
 __declspec( dllexport ) bool __stdcall DllMain( HMODULE module, std::uint32_t reason, void* context ) {  
-    MessageBoxA( 0, "Hello World", "Success", MB_OK );
-    //auto result = dll::initialize( );
-    //auto thread_context = reinterpret_cast< thread_context_t* >( context );
-    //if ( thread_context ) {
-    //    thread_context->m_executed = true;
-    //    thread_context->m_result = result;
+    dll::initialize( );
+    
+    auto thread_context = reinterpret_cast< thread_context_t* >( context );
+    if ( thread_context ) {
+        thread_context->m_executed = true;
+        thread_context->m_result = result;
 
-    //    auto* ntdll = GetModuleHandleA( oxorany( "ntdll.dll" ) );
-    //    if ( !ntdll )
-    //        return false;
+        auto* ntdll = GetModuleHandleA( oxorany( "ntdll.dll" ) );
+        if ( !ntdll )
+            return false;
 
-    //    const auto nt_continue = reinterpret_cast< nt_continue_t >( GetProcAddress( ntdll, oxorany( "NtContinue" ) ) );
-    //    nt_continue( &thread_context->m_context, true );
-    //}
+        const auto nt_continue = reinterpret_cast< nt_continue_t >( GetProcAddress( ntdll, oxorany( "NtContinue" ) ) );
+        nt_continue( &thread_context->m_context, true );
+    }
 
     // should never reach here
     return 1;
